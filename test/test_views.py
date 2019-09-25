165e0bc44cdf7417fa9af29038c0abf5022d2efb
julie <julie.malcuit@laposte.net>
2019-09-25 17:01:52 +0200

    fin

--- test/test_views.py
+++ test/test_views.py
@@ -7,13 +7,33 @@ class TestViews(TestCase):
         app.config['TESTING'] = True
         return app
 
-#    def test_products_json(self):
-#        response = self.client.get("/api/v1/products")
-#        products = response.json
-#        self.assertIsInstance(products, list)
-#        self.assertGreater(len(products), 2) # 2 is not a mistake here.
-
-    def test_products_r(self):
-        response = self.client.get('GET /api/v1/products/:1')
+    def test_read_products(self):
+        response = self.client.get("/api/v1/products")
         products = response.json
-        self.assertEquals ((products[4]), "Skello")
+        self.assertIsInstance(products, list)
+        self.assertGreater(len(products), 2) # 2 is not a mistake here.
+
+    def test_read_product_success(self):
+        response = self.client.get("/api/v1/products/1")
+        product = response.json
+        self.assertEqual(product['id'], 1)
+
+    def test_read_product_fail(self):
+        response = self.client.get("/api/v1/products/9")
+        self.assertEqual(response.status_code, 404)
+
+    def test_delete_product_success(self):
+        first_response = self.client.get("/api/v1/products")
+        first_products = first_response.json
+        self.assertEqual(len(first_products), 3)
+
+        response = self.client.get("/api/v1/products/1")
+        self.assertEqual(response.status_code, 204)
+
+        second_response = self.client.get("/api/v1/products")
+        second_products = second_response.json
+        self.assertEqual(len(second_products), 2)
+
+    def test_delete_product_fail(self):
+        response = self.client.get("/api/v1/products/9")
+        self.assertEqual(response.status_code, 404)
--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,5 @@
 # wsgi.py
-from flask import Flask
+from flask import Flask, abort, request
 from flask import jsonify
 app = Flask(__name__)
 
@@ -9,14 +9,23 @@ PRODUCTS = [
     { 'id': 3, 'name': 'Other' }
 ]
 
-#@app.route('/api/v1/products')
-#def products():
-#    return jsonify(PRODUCTS)
+'''Affiche le dict products'''
+@app.route('/api/v1/products')
+def products():
+    return jsonify(PRODUCTS)
 
-@app.route('GET /api/v1/products/:<int:post_id>')
-    def products(post_id):
-        id = post_id
-        if id == True:
-            return PRODUCTS[id, 'name']
-        else:
-            return "error 404"
+'''GET'''
+@app.route('/api/v1/products/<int:post_id>', methods=['GET'])
+def products_read(post_id):
+    for product in PRODUCTS:
+        if post_id == product['id']:
+            return jsonify(product)
+    abort (404)
+
+'''DELETE'''
+@app.route('/api/v1/products/<int:post_id>', methods=['DELETE'])
+def products_delete(post_id):
+        for product in PRODUCTS:
+            if post_id == product['id']:
+                del product['id']
+                abort(204)
